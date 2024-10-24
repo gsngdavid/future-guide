10fbfd1c29d834d1a7ea83a1c87207f10989e669
David Gusenga <gsngdavid@gmail.com>
2024-10-24 08:58:19 +0200

    chore: change skeleton

--- components/Messages.tsx
+++ components/Messages.tsx
@@ -13,10 +13,10 @@ export default function Messages({ isLoading, messages }: ContentProps) {
     useScrollToBottom<HTMLDivElement>(messages, isLoading);
 
   return isLoading ? (
-    <div className="mt-8 flex flex-col gap-y-2">
-      <div className="h-[30px] animate-pulse bg-black/10" />
-      <div className="h-[30px] animate-pulse bg-black/10" />
-      <div className="h-[30px] animate-pulse bg-black/10" />
+    <div className="mt-8 flex flex-col gap-y-2 mb-10">
+      <div className="h-[30px] animate-pulse bg-slate-700" />
+      <div className="h-[30px] animate-pulse bg-slate-700" />
+      <div className="h-[30px] animate-pulse bg-slate-700" />
     </div>
   ) : (
     <div className="h-[calc(100%-130px)] mb-10 my-4 overflow-y-scroll pr-7">
